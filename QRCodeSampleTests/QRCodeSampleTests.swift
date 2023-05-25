843618577372200e38e43c5634a267cde17bda7f
tibita11 <tibita0811@gmail.com>
2023-05-25 17:14:26 +0900

    test: DB保存処理のユニットテストを追加

--- QRCodeSampleTests/QRCodeSampleTests.swift
+++ QRCodeSampleTests/QRCodeSampleTests.swift
@@ -7,29 +7,15 @@
 
 import XCTest
 import Photos
+import RealmSwift
 @testable import QRCodeSample
 
 final class QRCodeGeneratorTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func testGenerateQRCode() {
         XCTContext.runActivity(named: "QRコードが作成されること") { _ in
             let image = QRCodeGeneratorViewModel().generateQRCode(value: "テスト")
             XCTAssertNotNil(image)
-        }
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 }
@@ -52,6 +38,43 @@ final class QRCodeReaderTests: XCTestCase {
             guard let testImage = UIImage(systemName: "person.fill") else { return }
             let message = qrCodeReaderViewModel.detectQRCode(from: testImage)
             XCTAssertNil(message)
+        }
+    }
+}
+
+
+// MARK: - QRCodeAdditionTests
+
+final class QRCodeAdditionTests: XCTestCase {
+    private var dataStorage: DataStorage!
+    private var realm: Realm!
+    
+    override func setUp() {
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+        realm = try! Realm()
+        dataStorage = DataStorage(realm: realm)
+    }
+    
+    func testSave() {
+        XCTContext.runActivity(named: "Listが存在しない場合に新規Listを作成し、追加されること") { _ in
+            dataStorage.save(value: "テスト1")
+            guard let list = realm.objects(ItemList.self).first?.list, let firstItem = list.first else {
+                XCTFail("DBへの登録が正しくありません。")
+                return
+            }
+            XCTAssertEqual(list.count, 1)
+            XCTAssertEqual(firstItem.title, "テスト1")
+        }
+        
+        XCTContext.runActivity(named: "Listが存在する場合に新規データがlistに追加されること") { _ in
+            dataStorage.save(value: "テスト2")
+            guard let list = realm.objects(ItemList.self).first?.list, let firstItem = list.first, let lastItem = list.last else {
+                XCTFail("DBへの登録が正しくありません。")
+                return
+            }
+            XCTAssertEqual(list.count, 2)
+            XCTAssertEqual(firstItem.title, "テスト1")
+            XCTAssertEqual(lastItem.title, "テスト2")
         }
     }
 }
